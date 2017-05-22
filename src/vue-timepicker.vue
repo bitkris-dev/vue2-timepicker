5290b2a4e126246663c7e8910b68e51b0c0f8cfc
Kristian Barrese <kristian.webdeveloper@gmail.com>
2017-05-23 01:45:30 +0200

    wineplan custom classes

--- src/vue-timepicker.vue
+++ src/vue-timepicker.vue
@@ -373,7 +373,7 @@ export default {
 
 <template>
 <span class="time-picker">
-  <input class="display-time" :id="id" v-model="displayTime" @click.stop="toggleDropdown" type="text" readonly />
+  <input class="display-time boxshadow1 form-control form-control-big input-icon input-icon-time" :id="id" v-model="displayTime" @click.stop="toggleDropdown" type="text" readonly />
   <span class="clear-btn" v-if="!hideClearButton" v-show="!showDropdown && showClearBtn" @click.stop="clearTime">&times;</span>
   <div class="time-picker-overlay" v-if="showDropdown" @click.stop="toggleDropdown"></div>
   <div class="dropdown" v-show="showDropdown">
